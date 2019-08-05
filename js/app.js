a53b1833b0239bc6c2e127deaa023689385c5faf
Sylwia Fait <sylwia.fait@gmail.com>
2019-08-05 12:44:16 +0200

    Update menu

--- js/app.js
+++ js/app.js
@@ -3,23 +3,25 @@ function showMenu() {
     const secondMenu = document.querySelectorAll(".second-menu");
 
     for (let i = 0; i < firstMenu.length; i++) {
-        firstMenu[i].addEventListener("mouseover", function() {
+        firstMenu[i].addEventListener("click", function() {
             this.querySelector(".second-menu").style.display = "block";
         })
 
-        firstMenu[i].addEventListener("mouseout", function() {
+        firstMenu[i].addEventListener("click", function() {
             this.querySelector(".second-menu").style.display = "none";
         })
     }
 }
 
 function showHamburgerMenu() {
-    const hamburger = document.querySelector('.hamburger');
+    const hamburger = document.querySelector(".hamburger");
+    const mainHeader  = document.querySelector(".main-header");
     const menu = document.querySelector(".menu");
 
     hamburger.addEventListener("click", function () {
         // icon.classList.toggle("active");
         menu.classList.toggle("show-menu");
+        mainHeader.classList.toggle("hide-header");
     })
 }
 
