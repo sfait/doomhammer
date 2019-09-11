93e0a7c5b9bb4eda8c773ce5b3255ffb70569268
Sylwia Fait <sylwia.fait@gmail.com>
2019-09-11 19:52:31 +0200

    Update second-menu

--- js/app.js
+++ js/app.js
@@ -3,16 +3,8 @@ function showMenu() {
     const secondMenu = document.querySelectorAll(".second-menu");
 
     for (let i = 0; i < firstMenu.length; i++) {
-        firstMenu[i].addEventListener("mouseover", function() {
-            this.querySelector(".second-menu").style.display = "block";
-        })
-
         firstMenu[i].addEventListener("click", function() {
             this.querySelector(".second-menu").classList.toggle('show-menu');
-        })
-
-        firstMenu[i].addEventListener("mouseout", function() {
-            this.querySelector(".second-menu").style.display = "none";
         })
     }
 }
